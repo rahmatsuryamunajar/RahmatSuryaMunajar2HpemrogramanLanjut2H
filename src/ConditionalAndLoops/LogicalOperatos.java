9862da45eee2f70b958a996abeb69b2ba9a89e8d
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 15:12:10 +0700

    project sololearn.com dari intellij Idea

---
+++ src/ConditionalAndLoops/LogicalOperatos.java
@@ -0,0 +1,15 @@
+package ConditionalAndLoops;
+
+public class LogicalOperatos {
+    public static void main(String[] args) {
+        int age = 23;
+
+        int money = 4000;
+
+        if (age > 21 && money > 500) {
+
+            System.out.println("Welcome");
+
+        }
+    }
+}
