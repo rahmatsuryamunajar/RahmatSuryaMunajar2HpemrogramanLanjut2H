8ac150773e8d8d0cefb19cfa1eb1d3bb2670798e
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 14:53:14 +0700

    project sololearn.com dari intellij Idea

---
+++ src/ConditionalAndLoops/DecisionMaking.java
@@ -0,0 +1,14 @@
+package ConditionalAndLoops;
+
+public class DecisionMaking {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if
+        ( x == 5 ) {
+
+            System.out.println("Yeah");
+
+        }
+    }
+}
