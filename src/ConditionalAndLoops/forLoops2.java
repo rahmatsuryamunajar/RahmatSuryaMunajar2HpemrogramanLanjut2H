7a1cee04b00a657f721f38715126d4d84d04d23b
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 15:31:54 +0700

    project sololearn.com dari intellij Idea

---
+++ src/ConditionalAndLoops/forLoops2.java
@@ -0,0 +1,9 @@
+package ConditionalAndLoops;
+
+public class forLoops2 {
+    public static void main(String[] args) {
+        for (int i = 2; i < 10; i = i*i) {
+            System.out.println(i);
+        }
+    }
+}
