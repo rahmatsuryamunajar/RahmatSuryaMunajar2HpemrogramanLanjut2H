3edd90b7f97b8f23c5d833840f8fee110eef1dcb
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 18:32:22 +0700

    project sololearn.com dari intellij Idea

---
+++ src/ClassesAndObjects/methods.java
@@ -0,0 +1,12 @@
+package ClassesAndObjects;
+
+public class methods {
+    public static void main(String[] args) {
+        hello();
+    }
+    static void hello() {
+
+        System.out.println("hi");
+
+    }
+}
