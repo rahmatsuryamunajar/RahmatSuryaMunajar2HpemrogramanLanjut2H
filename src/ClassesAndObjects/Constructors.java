85493517a1c0c8484fbc2092c99c930ff00dd489
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 23:11:58 +0700

    project sololearn.com dari intellij Idea

---
+++ src/ClassesAndObjects/Constructors.java
@@ -0,0 +1,12 @@
+package ClassesAndObjects;
+
+public class Constructors {
+    public static void main(String[] args) {
+        class person{
+            private int age;
+            public person(int myage){
+                age = myage;
+            }
+        }
+    }
+}
