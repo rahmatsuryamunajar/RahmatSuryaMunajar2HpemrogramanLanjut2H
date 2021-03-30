753a78fb57d09223cc0d13a47e8f5162f761930c
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 15:07:01 +0700

    project sololearn.com dari intellij Idea

---
+++ src/ConditionalAndLoops/NestedifStatements.java
@@ -0,0 +1,16 @@
+package ConditionalAndLoops;
+
+public class NestedifStatements {
+    public static void main(String[] args) {
+        int x = 37;
+
+        if (x > 22) {
+            if (x > 31) {
+
+                System.out.println("it works!");
+
+            }
+
+        }
+    }
+}
