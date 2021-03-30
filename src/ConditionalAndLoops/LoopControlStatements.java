77bc2c51204f80ca63706dfb80786bdf9c066c76
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 16:28:49 +0700

    project sololearn.com dari intellij Idea

---
+++ src/ConditionalAndLoops/LoopControlStatements.java
@@ -0,0 +1,13 @@
+package ConditionalAndLoops;
+
+public class LoopControlStatements {
+    public static void main(String[] args) {
+        int x = 1;
+
+        do { System.out.println(x);
+        x++;
+
+        }
+        while (x <= 5);
+    }
+}
