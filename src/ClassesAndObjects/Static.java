c704a2cbee09d1e01f70d13478f0c600e9462df7
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 23:46:57 +0700

    project sololearn.com dari intellij Idea

---
+++ src/ClassesAndObjects/Static.java
@@ -0,0 +1,11 @@
+package ClassesAndObjects;
+
+public class Static {
+    public static int pCount;
+
+    public static void main(String[] args) {
+        Static.pCount = 1;
+        Static.pCount++;
+        System.out.println(Static.pCount);
+    }
+}
