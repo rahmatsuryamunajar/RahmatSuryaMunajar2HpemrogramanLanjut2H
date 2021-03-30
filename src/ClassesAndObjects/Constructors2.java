bf30c55cb9e70d4cda6f2d3dc323b92df8b613ae
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 23:34:40 +0700

    project sololearn.com dari intellij Idea

---
+++ src/ClassesAndObjects/Constructors2.java
@@ -0,0 +1,14 @@
+package ClassesAndObjects;
+
+public class Constructors2 {
+    public static void main(String[] args) {
+        class A
+        {
+            private int x;
+            public A(
+                    int val) {
+                x = val;
+            }
+        }
+    }
+}
