d5ab31ed50586404e3681322cd371bc1992a0951
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 17:41:57 +0700

    project sololearn.com dari intellij Idea

---
+++ src/Arrays/MultidimensionalArrays.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+public class MultidimensionalArrays {
+    public static void main(String[] args) {
+        String array[ ][ ] =  { {"Hey", "John", "Bye"},
+                {"John", "Johnson", "Hello"} };
+        System.out.println(array[1][1]);
+    }
+}
