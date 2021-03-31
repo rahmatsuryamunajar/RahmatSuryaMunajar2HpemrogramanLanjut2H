f2b2cdac4efd5a4ecf65bcf6823e95a2b3f22d91
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-31 08:12:15 +0700

    project sololearn.com dari intellij Idea

---
+++ src/BasicConsept/Variables.java
@@ -0,0 +1,9 @@
+package BasicConsept;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 24;
+        double height = 189.87;
+    }
+}
